c3aee5cd301e90779a693d9fd83515ace84b03e3
Bryan Chau <bchau2020@fau.edu>
2025-05-29 00:12:03 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extentions = ['.com', '.us', '.net', '.io'];
+
+   let result = [];
+
+    for (let p of pronoun) {
+        for (let a of adj) {
+            for (let n of noun) {
+              for (let e of extentions){
+                result.push(p + a + n + e);
+              }
+            }
+        }
+    }
+
+    let domain = result[Math.floor(Math.random() * result.length)];
+
+  /* Different way
+  let randpro = pronoun[Math.floor(Math.random()*pronoun.length)];
+  let randadj = adj[Math.floor(Math.random()*adj.length)];
+  let rannoun = noun[Math.floor(Math.random()*noun.length)];
+
+  let domain = randpro + randadj + rannoun + ".com";
+*/
+  document.getElementById("domain").innerHTML = domain;
 };
